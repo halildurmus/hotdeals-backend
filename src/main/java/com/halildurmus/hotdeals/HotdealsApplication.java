6fcc7e52affba284ed090cbd86977bb32c967517
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-03-31 23:56:16 +0300

    Initial commit

---
+++ src/main/java/com/halildurmus/hotdeals/HotdealsApplication.java
@@ -0,0 +1,13 @@
+package com.halildurmus.hotdeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotdealsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HotdealsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/halildurmus/hotdeals/HotdealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halildurmus.hotdeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotdealsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
