fd2e4071f42115044331dc7fea41611f77ba19a1
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-04-05 21:54:34 +0300

    Enable mongodb validation

---
+++ src/main/java/com/halildurmus/hotdeals/config/MongoValidationConfig.java
@@ -0,0 +1,15 @@
+package com.halildurmus.hotdeals.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class MongoValidationConfig {
+
+  @Bean
+  public ValidatingMongoEventListener validatingMongoEventListener(LocalValidatorFactoryBean factory) {
+    return new ValidatingMongoEventListener(factory);
+  }
+}
