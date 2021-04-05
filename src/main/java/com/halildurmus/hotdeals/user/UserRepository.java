2a8c19a0a6eb9b42ca5bf70056df3d1c8b32d818
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-04-05 21:56:27 +0300

    Add UserRepository

---
+++ src/main/java/com/halildurmus/hotdeals/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.halildurmus.hotdeals.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
