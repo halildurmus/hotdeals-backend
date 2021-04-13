321338fa9363454ffd78078c84f4efffe9bdfbf9
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-04-14 00:33:23 +0300

    Add integration tests for DealRepository

---
+++ src/test/java/com/halildurmus/hotdeals/deal/dummy/DummyDeals.java
@@ -0,0 +1,11 @@
+package com.halildurmus.hotdeals.deal.dummy;
+
+import com.halildurmus.hotdeals.deal.Deal;
+
+public class DummyDeals {
+
+  public static Deal deal1 = new Deal("12TB WD My Book Desktop External HDD",
+      "12TB WD My Book Desktop External HDD\n$190",
+      "http://www.gravatar.com/avatar");
+
+}
