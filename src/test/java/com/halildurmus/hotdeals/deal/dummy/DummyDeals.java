624a934ebf0147ed0d878dccebe1a265c224fdce
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-04-15 19:40:07 +0300

    Fix tests

--- src/test/java/com/halildurmus/hotdeals/deal/dummy/DummyDeals.java
+++ src/test/java/com/halildurmus/hotdeals/deal/dummy/DummyDeals.java
@@ -1,11 +1,12 @@
 package com.halildurmus.hotdeals.deal.dummy;
 
 import com.halildurmus.hotdeals.deal.Deal;
+import org.bson.types.ObjectId;
 
 public class DummyDeals {
 
   public static Deal deal1 = new Deal("12TB WD My Book Desktop External HDD",
-      "12TB WD My Book Desktop External HDD\n$190",
+      "12TB WD External HDD\n$190", new ObjectId("5fbe790ec6f0b32014074bb1"),
       "http://www.gravatar.com/avatar");
 
 }
