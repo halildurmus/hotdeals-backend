337612fb01030892cabd180fc1688362ccf35148
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-04-16 17:16:04 +0300

    Expose Category ids

--- src/main/java/com/halildurmus/hotdeals/config/RepositoryConfig.java
+++ src/main/java/com/halildurmus/hotdeals/config/RepositoryConfig.java
@@ -1,10 +1,11 @@
 package com.halildurmus.hotdeals.config;
 
+import com.halildurmus.hotdeals.category.Category;
 import com.halildurmus.hotdeals.deal.Deal;
 import com.halildurmus.hotdeals.store.Store;
 import com.halildurmus.hotdeals.user.User;
 import org.springframework.context.annotation.Configuration;
-    import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
@@ -12,8 +13,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 class RepositoryConfig implements RepositoryRestConfigurer {
 
   @Override
-  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-    config.exposeIdsFor(Deal.class, Store.class, User.class);
+  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+      CorsRegistry cors) {
+    config.exposeIdsFor(Category.class, Deal.class, Store.class, User.class);
   }
 
 }
