66f4d6b5c0d492832ba0fda9ca2552646ae5f289
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-05-10 21:55:44 +0300

    Update SecurityConfig

--- src/main/java/com/halildurmus/hotdeals/security/role/RoleController.java
+++ src/main/java/com/halildurmus/hotdeals/security/role/RoleController.java
@@ -1,12 +1,10 @@
 package com.halildurmus.hotdeals.security.role;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +16,6 @@ public class RoleController {
   private RoleService roleService;
 
   @PutMapping
-  @ResponseStatus(value = HttpStatus.CREATED)
   public void addRole(@RequestParam String uid, @RequestParam String role) throws Exception {
     roleService.addRole(uid, role);
   }
