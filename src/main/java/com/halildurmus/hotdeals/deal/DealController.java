a249bc6637b3d4fc6bd64f69594c23036dc37564
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-05-20 16:58:58 +0300

    Use ObjectUtils.isEmpty for checking null

--- src/main/java/com/halildurmus/hotdeals/deal/DealController.java
+++ src/main/java/com/halildurmus/hotdeals/deal/DealController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,19 +15,17 @@ public class DealController {
   private DealService service;
 
   @PostMapping("/deals/vote")
-  public ResponseEntity<Deal> vote(@RequestBody Map<String, String> json)
+  public Deal vote(@RequestBody Map<String, String> json)
       throws Exception {
     final String dealId = json.get("dealId");
     final String userId = json.get("userId");
     final String voteType = json.get("voteType");
 
-    if (dealId == null || userId == null || voteType == null) {
+    if (ObjectUtils.isEmpty(dealId) || ObjectUtils.isEmpty(userId) || ObjectUtils.isEmpty(voteType)) {
       throw new Exception("dealId, userId and voteType fields cannot be blank!");
     }
 
-    Deal response = service.vote(dealId, userId, voteType);
-
-    return ResponseEntity.status(200).body(response);
+    return service.vote(dealId, userId, voteType);
   }
 
 }
