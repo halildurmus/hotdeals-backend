33b4557fea9dc3b7000f0244870ba82a6e94ce3a
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-06-03 15:20:31 +0300

    Fix dummy category

--- src/test/java/com/halildurmus/hotdeals/category/dummy/DummyCategories.java
+++ src/test/java/com/halildurmus/hotdeals/category/dummy/DummyCategories.java
@@ -1,9 +1,13 @@
 package com.halildurmus.hotdeals.category.dummy;
 
 import com.halildurmus.hotdeals.category.Category;
+import java.util.Map;
 
 public class DummyCategories {
 
-  public static Category category1 = new Category("Computers", "/", "/computers");
+  public static Category category1 = new Category("Computers", "/", "/computers", Map.of(
+      "codePoint", "57803",
+      "fontFamily", "MaterialIcons"
+  ));
 
 }
