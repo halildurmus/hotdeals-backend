c7f08d1fe1dc8259e16a732485bbf6d7a122b61a
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-09-17 22:51:05 +0300

    Update RedisConfig

--- src/main/java/com/halildurmus/hotdeals/HotdealsApplication.java
+++ src/main/java/com/halildurmus/hotdeals/HotdealsApplication.java
@@ -2,10 +2,8 @@ package com.halildurmus.hotdeals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class HotdealsApplication {
 
   public static void main(String[] args) { SpringApplication.run(HotdealsApplication.class, args); }
--- src/main/java/com/halildurmus/hotdeals/config/RedisConfig.java
+++ src/main/java/com/halildurmus/hotdeals/config/RedisConfig.java
@@ -1,17 +1,38 @@
 package com.halildurmus.hotdeals.config;
 
 import java.time.Duration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
   @Bean
   public RedisCacheConfiguration cacheConfiguration() {
-    return RedisCacheConfiguration.defaultCacheConfig().disableCachingNullValues()
+    return RedisCacheConfiguration.defaultCacheConfig()
+        .disableCachingNullValues()
         .entryTtl(Duration.ofMinutes(10));
   }
 
+  @Bean
+  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(cf);
+
+    return redisTemplate;
+  }
+
+  @Bean
+  public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+    return RedisCacheManager
+        .builder(redisConnectionFactory)
+        .cacheDefaults(cacheConfiguration()).build();
+  }
 }
