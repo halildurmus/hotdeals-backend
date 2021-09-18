00e34ed5bfb55753d86da4c7c6e5a8879eaebc31
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-09-19 02:24:25 +0300

    Configure redis cache in application.yml

--- src/main/java/com/halildurmus/hotdeals/config/RedisConfig.java
+++ src/main/java/com/halildurmus/hotdeals/config/RedisConfig.java
@@ -1,29 +1,22 @@
 package com.halildurmus.hotdeals.config;
 
-import java.time.Duration;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
-  @Bean
-  public RedisCacheConfiguration cacheConfiguration() {
-    return RedisCacheConfiguration.defaultCacheConfig()
-        .disableCachingNullValues()
-        .entryTtl(Duration.ofMinutes(10));
-  }
+//  @Bean
+//  public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+//    final RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+//        .disableCachingNullValues()
+//        .entryTtl(Duration.ofMinutes(10));
+//
+//    return RedisCacheManager
+//        .builder(connectionFactory)
+//        .cacheDefaults(cacheConfiguration)
+//        .build();
+//  }
 
-  @Bean
-  public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-    return RedisCacheManager
-        .builder(redisConnectionFactory)
-        .cacheDefaults(cacheConfiguration()).build();
-  }
 }
