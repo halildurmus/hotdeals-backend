20557945661c5a6aae91e2cf5ef1ce4d2f4316d7
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-09-20 00:01:42 +0300

    Refactor SecurityFilter

--- src/main/java/com/halildurmus/hotdeals/security/SecurityFilter.java
+++ src/main/java/com/halildurmus/hotdeals/security/SecurityFilter.java
@@ -12,7 +12,6 @@ import com.halildurmus.hotdeals.user.UserRepository;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -53,66 +52,64 @@ public class SecurityFilter extends OncePerRequestFilter {
   }
 
   public String parseBearerToken(HttpServletRequest request) {
-    String bearerToken = null;
     String authHeader = request.getHeader("Authorization");
-
-    if (ObjectUtils.isNotEmpty(authHeader) && authHeader.startsWith("Bearer ")
-        && authHeader.length() > 50) {
-      bearerToken = authHeader.substring(7);
+    if (ObjectUtils.isEmpty(authHeader) || authHeader.startsWith("Bearer ")) {
+      return null;
     }
 
-    return bearerToken;
+    return authHeader.substring(7);
   }
 
   private void verifyToken(HttpServletRequest request) {
-    FirebaseToken decodedToken = null;
     String token = parseBearerToken(request);
     logger.info("Token: " + token);
-
+    FirebaseToken decodedToken = null;
     try {
       if (ObjectUtils.isNotEmpty(token)) {
         decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
       }
     } catch (FirebaseAuthException e) {
-      e.printStackTrace();
-      log.error("Firebase Exception:: {}", e.getLocalizedMessage());
+      log.error("Firebase Exception: {}", e.getLocalizedMessage());
+    }
+
+    if (decodedToken == null) {
+      return;
     }
 
     User user = firebaseTokenToUserDto(decodedToken);
-    List<GrantedAuthority> authorities = new ArrayList<>();
-
-    if (user != null && decodedToken != null) {
-      // Handle Super role
-      if (securityProps.getSuperAdmins() != null && securityProps.getSuperAdmins()
-          .contains(user.getEmail())) {
-        if (!decodedToken.getClaims().containsKey(RoleConstants.ROLE_SUPER)) {
-          try {
-            roleService.addRole(decodedToken.getUid(), RoleConstants.ROLE_SUPER);
-          } catch (Exception e) {
-            log.error("Super Role registration exception ", e);
-          }
-        }
-        authorities.add(new SimpleGrantedAuthority(RoleConstants.ROLE_SUPER));
-      }
-      // Handle other roles
-      decodedToken.getClaims().forEach((k, v) -> authorities.add(new SimpleGrantedAuthority(k)));
-
-      UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-          user,
-          new Credentials(decodedToken, token), authorities);
-      authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-      SecurityContextHolder.getContext().setAuthentication(authentication);
+    if (user == null) {
+      return;
     }
+
+    List<GrantedAuthority> authorities = new ArrayList<>();
+    // Handle ROLE_SUPER
+    if (securityProps.getSuperAdmins() != null && securityProps.getSuperAdmins()
+        .contains(user.getEmail())) {
+      if (!decodedToken.getClaims().containsKey(RoleConstants.ROLE_SUPER)) {
+        try {
+          roleService.addRole(decodedToken.getUid(), RoleConstants.ROLE_SUPER);
+        } catch (Exception e) {
+          log.error("Failed to add ROLE_SUPER to the user", e);
+        }
+      }
+      authorities.add(new SimpleGrantedAuthority(RoleConstants.ROLE_SUPER));
+    }
+    // Handle other roles
+    decodedToken.getClaims().forEach((k, v) -> authorities.add(new SimpleGrantedAuthority(k)));
+
+    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+        user,
+        new Credentials(decodedToken, token), authorities);
+    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+    SecurityContextHolder.getContext().setAuthentication(authentication);
   }
 
   private User firebaseTokenToUserDto(FirebaseToken decodedToken) {
-    Optional<User> user = Optional.empty();
-
-    if (decodedToken != null) {
-      user = userRepository.findByUid(decodedToken.getUid());
+    if (decodedToken == null) {
+      return null;
     }
 
-    return user.orElse(null);
+    return userRepository.findByUid(decodedToken.getUid()).orElse(null);
   }
 
 }
