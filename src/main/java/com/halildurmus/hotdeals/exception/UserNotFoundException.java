e943b1c58873f6c7b5f66859ca072872aaf2375b
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-09-28 22:44:45 +0300

    Add missing UserNotFoundException

---
+++ src/main/java/com/halildurmus/hotdeals/exception/UserNotFoundException.java
@@ -0,0 +1,25 @@
+package com.halildurmus.hotdeals.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+  public UserNotFoundException() {
+  }
+
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+
+  public UserNotFoundException(Throwable cause) {
+    super(cause);
+  }
+
+  public UserNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public UserNotFoundException(String message, Throwable cause, boolean enableSuppression,
+      boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+
+}
