848c9d6e719b3a5f7863b07216c491974daa0533
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-01 20:16:09 +0300

    Add pagination support to findByDealIdOrderByCreatedAtDesc

--- src/main/java/com/halildurmus/hotdeals/comment/CommentRepository.java
+++ src/main/java/com/halildurmus/hotdeals/comment/CommentRepository.java
@@ -1,12 +1,12 @@
 package com.halildurmus.hotdeals.comment;
 
-import java.util.List;
-import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -36,6 +36,6 @@ public interface CommentRepository extends MongoRepository<Comment, String> {
   int countCommentsByPostedBy(ObjectId postedBy);
 
   @Cacheable(value = "comments:findByDealIdOrderByCreatedAtDesc", key = "#dealId", condition = "#dealId != null")
-  Optional<List<Comment>> findByDealIdOrderByCreatedAtDesc(ObjectId dealId);
+  Page<Comment> findByDealIdOrderByCreatedAtDesc(ObjectId dealId, Pageable pageable);
 
 }
