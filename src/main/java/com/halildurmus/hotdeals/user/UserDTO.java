a7e387d08bed748a2c54159724c64b196b1dda15
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-02 15:55:38 +0300

    Add UserDTO

---
+++ src/main/java/com/halildurmus/hotdeals/user/UserDTO.java
@@ -0,0 +1,19 @@
+package com.halildurmus.hotdeals.user;
+
+import java.io.Serializable;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserDTO implements Serializable {
+
+  private static final long serialVersionUID = 1234567L;
+
+  private String id;
+
+  private String avatar;
+
+  private String nickname;
+
+}
