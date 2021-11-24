2f733ac8159fa594c8ebb678fe9933f6e4e7f143
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-11-24 22:01:20 +0300

    Handle exception

--- src/main/java/com/halildurmus/hotdeals/notification/NotificationController.java
+++ src/main/java/com/halildurmus/hotdeals/notification/NotificationController.java
@@ -1,7 +1,10 @@
 package com.halildurmus.hotdeals.notification;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +17,15 @@ public class NotificationController {
   private NotificationService notificationService;
 
   @PostMapping("/notifications")
-  public int sendNotification(@RequestBody Note note, @RequestParam List<String> tokens)
-      throws Exception {
-    return notificationService.sendNotification(note, tokens);
+  public ResponseEntity<Object> sendNotification(@RequestBody Note note,
+      @RequestParam List<String> tokens) {
+    try {
+      final int response = notificationService.sendNotification(note, tokens);
+
+      return ResponseEntity.status(201).body(response);
+    } catch (FirebaseMessagingException e) {
+      return ResponseEntity.status(400).body(HttpStatus.BAD_REQUEST);
+    }
   }
 
 }
