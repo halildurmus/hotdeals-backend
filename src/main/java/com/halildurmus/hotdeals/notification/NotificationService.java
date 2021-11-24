2bcc0c00efdbe1945da781783c8a3fa2c409d90b
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-11-24 22:01:00 +0300

    Set authenticated user's id to notification's actor

--- src/main/java/com/halildurmus/hotdeals/notification/NotificationService.java
+++ src/main/java/com/halildurmus/hotdeals/notification/NotificationService.java
@@ -1,9 +1,10 @@
 package com.halildurmus.hotdeals.notification;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
 import java.util.List;
 
 public interface NotificationService {
 
-  int sendNotification(Note note, List<String> tokens) throws Exception;
+  int sendNotification(Note note, List<String> tokens) throws FirebaseMessagingException;
 
 }
--- src/main/java/com/halildurmus/hotdeals/notification/NotificationServiceImpl.java
+++ src/main/java/com/halildurmus/hotdeals/notification/NotificationServiceImpl.java
@@ -3,12 +3,14 @@ package com.halildurmus.hotdeals.notification;
 import com.google.firebase.messaging.BatchResponse;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
 import com.google.firebase.messaging.SendResponse;
+import com.halildurmus.hotdeals.security.SecurityService;
+import com.halildurmus.hotdeals.user.User;
 import com.halildurmus.hotdeals.user.UserService;
 import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,25 +23,33 @@ public class NotificationServiceImpl implements NotificationService {
   private FirebaseMessaging firebaseMessaging;
 
   @Autowired
+  private SecurityService securityService;
+
+  @Autowired
   private UserService userService;
 
   public int sendNotification(Note note, List<String> tokens) throws FirebaseMessagingException {
-    Notification notification = Notification
+    final User user = securityService.getUser();
+    final Map<String, String> data = note.getData();
+    data.put("actor", user.getId());
+
+    final Notification notification = Notification
         .builder()
         .setTitle(note.getTitle())
         .setBody(note.getBody())
         .setImage(note.getImage())
         .build();
 
-    MulticastMessage message = MulticastMessage.builder()
+    final MulticastMessage message = MulticastMessage.builder()
         .setNotification(notification)
         .putAllData(note.getData())
         .addAllTokens(tokens)
         .build();
 
-    BatchResponse batchResponse = firebaseMessaging.sendMulticast(message);
+    final BatchResponse batchResponse = firebaseMessaging.sendMulticast(message);
     log.debug(batchResponse.getSuccessCount() + " messages were sent successfully");
 
+    // Removes invalid FCM tokens
     for (int i = 0; i < batchResponse.getResponses().size(); i++) {
       final SendResponse sendResponse = batchResponse.getResponses().get(i);
       if (sendResponse.getException() != null) {
