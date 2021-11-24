188092838c355d2e69f690dc1f53ee98d73abda6
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-11-24 19:34:54 +0300

    Add missing annotations

--- src/main/java/com/halildurmus/hotdeals/exception/ExceptionResponse.java
+++ src/main/java/com/halildurmus/hotdeals/exception/ExceptionResponse.java
@@ -2,8 +2,14 @@ package com.halildurmus.hotdeals.exception;
 
 import java.time.LocalDateTime;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Builder
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class ExceptionResponse {
 
   private final LocalDateTime dateTime;
