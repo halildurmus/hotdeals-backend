3577d62679ac311282a3f67018c3a4787f476799
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-11-27 02:07:09 +0300

    Validate request parameter with @ObjectIdConstraint

--- src/main/java/com/halildurmus/hotdeals/comment/CommentController.java
+++ src/main/java/com/halildurmus/hotdeals/comment/CommentController.java
@@ -1,16 +1,18 @@
 package com.halildurmus.hotdeals.comment;
 
+import com.halildurmus.hotdeals.util.ObjectIdConstraint;
 import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.ObjectUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestController
+@Validated
 public class CommentController {
 
   @Autowired
@@ -18,11 +20,8 @@ public class CommentController {
 
   @GetMapping("/comments/search/findByDealId")
   public ResponseEntity<List<CommentDTO>> searchDeals(
-      @RequestParam(value = "dealId", defaultValue = "") String dealId,
+      @ObjectIdConstraint @RequestParam(value = "dealId", defaultValue = "") String dealId,
       Pageable pageable) {
-    if (ObjectUtils.isEmpty(dealId) || (!ObjectId.isValid(dealId))) {
-      throw new IllegalArgumentException("Invalid dealId!");
-    }
 
     final List<CommentDTO> response = service.getCommentsByDealId(new ObjectId(dealId), pageable);
 
