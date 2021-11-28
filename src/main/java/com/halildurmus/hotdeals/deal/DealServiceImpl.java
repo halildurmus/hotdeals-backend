60356f5c98692171a2ccd056b256f03cbf70c801
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-11-28 22:06:16 +0300

    Use Aggregation Update for vote deal

--- src/main/java/com/halildurmus/hotdeals/deal/DealServiceImpl.java
+++ src/main/java/com/halildurmus/hotdeals/deal/DealServiceImpl.java
@@ -1,5 +1,7 @@
 package com.halildurmus.hotdeals.deal;
 
+import static org.springframework.data.mongodb.core.aggregation.ArrayOperators.Filter.filter;
+import static org.springframework.data.mongodb.core.aggregation.ComparisonOperators.Ne.valueOf;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 
@@ -8,6 +10,7 @@ import com.halildurmus.hotdeals.deal.es.EsDealRepository;
 import com.halildurmus.hotdeals.exception.DealNotFoundException;
 import com.halildurmus.hotdeals.security.SecurityService;
 import com.halildurmus.hotdeals.user.User;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +18,10 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.AggregationUpdate;
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators.Subtract;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators.ConcatArrays;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators.Size;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
@@ -84,34 +91,37 @@ public class DealServiceImpl implements DealService {
     final ObjectId userId = new ObjectId(user.getId());
     final Deal deal = repository.findById(id)
         .orElseThrow(DealNotFoundException::new);
-    final List<ObjectId> upvoters = deal.getUpvoters();
-    final List<ObjectId> downvoters = deal.getDownvoters();
 
-    if (voteType.equals(VoteType.UP)) {
-      if (upvoters.contains(userId)) {
-        // TODO(halildurmus): Return HTTP 304 NOT MODIFIED
-        throw new Exception("You've already upvoted this deal before!");
-      }
-      downvoters.remove(userId);
-      upvoters.add(userId);
-    } else if (voteType.equals(VoteType.DOWN)) {
-      if (downvoters.contains(userId)) {
-        // TODO(halildurmus): Return HTTP 304 NOT MODIFIED
-        throw new Exception("You've already downvoted this deal before!");
-      }
-      upvoters.remove(userId);
-      downvoters.add(userId);
-    } else if (voteType.equals(VoteType.UNVOTE)) {
-      upvoters.remove(userId);
-      downvoters.remove(userId);
+    if (voteType.equals(VoteType.UP) && deal.getUpvoters().contains(userId)) {
+      // TODO(halildurmus): Return HTTP 304 NOT MODIFIED
+      throw new Exception("You've already upvoted this deal before!");
+    } else if (voteType.equals(VoteType.DOWN) && deal.getDownvoters().contains(userId)) {
+      // TODO(halildurmus): Return HTTP 304 NOT MODIFIED
+      throw new Exception("You've already downvoted this deal before!");
     }
 
-    final int upVoteCount = upvoters.size();
-    final int downVoteCount = downvoters.size();
-    deal.setDealScore(upVoteCount - downVoteCount);
-    repository.save(deal);
+    final Query query = query(where("_id").is(id));
+    final AggregationUpdate update = AggregationUpdate.update();
+    final FindAndModifyOptions options = FindAndModifyOptions.options().returnNew(true);
 
-    return deal;
+    if (voteType.equals(VoteType.UNVOTE)) {
+      update.set("upvoters").toValue(filter("upvoters").as("id")
+          .by(valueOf("id").notEqualToValue(userId)));
+      update.set("downvoters").toValue(filter("downvoters").as("id")
+          .by(valueOf("id").notEqualToValue(userId)));
+    } else {
+      final String fieldName1 = voteType.equals(VoteType.UP) ? "upvoters" : "downvoters";
+      final String fieldName2 = voteType.equals(VoteType.UP) ? "downvoters" : "upvoters";
+      update.set(fieldName1).toValue(ConcatArrays.arrayOf(fieldName1)
+          .concat(ConcatArrays.arrayOf(new ArrayList<>(List.of(userId)))));
+      update.set(fieldName2).toValue(filter(fieldName2).as("id")
+          .by(valueOf("id").notEqualToValue(userId)));
+    }
+
+    update.set("dealScore").toValue(Subtract.valueOf(Size.lengthOfArray("upvoters"))
+        .subtract(Size.lengthOfArray("downvoters")));
+
+    return mongoTemplate.findAndModify(query, update, options, Deal.class);
   }
 
 }
