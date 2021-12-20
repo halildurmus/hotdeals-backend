4fb7c1f41b53daf98131f0ac8099ab81b2e1bde3
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-12-21 01:20:58 +0300

    Make sure the parameters are  not empty

--- src/main/java/com/halildurmus/hotdeals/notification/NotificationController.java
+++ src/main/java/com/halildurmus/hotdeals/notification/NotificationController.java
@@ -2,11 +2,13 @@ package com.halildurmus.hotdeals.notification;
 
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @Validated
@@ -17,6 +19,10 @@ public class NotificationController {
 
   @PostMapping("/notifications")
   public ResponseEntity<Object> sendNotification(@Valid @RequestBody Note note) {
+    if (note.getTitleLocArgs().isEmpty() || note.getBodyLocArgs().isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+          "titleLocArgs and bodyLocArgs parameters cannot be empty");
+    }
     final int successCount = notificationService.sendNotification(note);
 
     return ResponseEntity.status(201).body(successCount);
