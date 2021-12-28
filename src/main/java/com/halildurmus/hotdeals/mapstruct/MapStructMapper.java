87d448ac61ba4669b7cfb1ed69e9158e8205cba1
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-12-28 18:34:20 +0300

    Update antMatchers

--- src/main/java/com/halildurmus/hotdeals/mapstruct/MapStructMapper.java
+++ src/main/java/com/halildurmus/hotdeals/mapstruct/MapStructMapper.java
@@ -3,8 +3,6 @@ package com.halildurmus.hotdeals.mapstruct;
 import com.halildurmus.hotdeals.comment.Comment;
 import com.halildurmus.hotdeals.comment.CommentGetDTO;
 import com.halildurmus.hotdeals.comment.CommentPostDTO;
-import com.halildurmus.hotdeals.post.Post;
-import com.halildurmus.hotdeals.post.PostDTO;
 import com.halildurmus.hotdeals.user.User;
 import com.halildurmus.hotdeals.user.UserGetDTO;
 import org.mapstruct.Mapper;
@@ -15,10 +13,6 @@ public interface MapStructMapper {
   Comment commentPostDtoToComment(CommentPostDTO commentPostDTO);
 
   CommentGetDTO commentToCommentGetDto(Comment comment);
-
-  Post postDtoToPost(PostDTO postDTO);
-
-  PostDTO postToPostDto(Post post);
 
   User userGetDtoToUser(UserGetDTO userGetDTO);
 
