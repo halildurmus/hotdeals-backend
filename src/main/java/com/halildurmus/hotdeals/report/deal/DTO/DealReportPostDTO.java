dfc3fd5608f552aec5aeaabee80b3a2f90d75086
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-12-31 17:03:31 +0300

    Remove reportedDeal property

--- src/main/java/com/halildurmus/hotdeals/report/deal/DTO/DealReportPostDTO.java
+++ src/main/java/com/halildurmus/hotdeals/report/deal/DTO/DealReportPostDTO.java
@@ -1,11 +1,9 @@
 package com.halildurmus.hotdeals.report.deal.DTO;
 
-import com.halildurmus.hotdeals.deal.Deal;
 import com.halildurmus.hotdeals.report.DTO.ReportDTO;
 import com.halildurmus.hotdeals.report.deal.DealReportReason;
 import java.util.List;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
@@ -14,9 +12,6 @@ import lombok.experimental.SuperBuilder;
 @Data
 @SuperBuilder
 public class DealReportPostDTO extends ReportDTO {
-
-  @NotNull
-  private Deal reportedDeal;
 
   @NotEmpty
   private List<DealReportReason> reasons;
