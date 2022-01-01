4e501d7aefbff8fe287c653312e4ec9b06e79cd6
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-01 23:04:41 +0300

    Add documentation to EnumUtil

--- src/main/java/com/halildurmus/hotdeals/util/EnumUtil.java
+++ src/main/java/com/halildurmus/hotdeals/util/EnumUtil.java
@@ -1,7 +1,18 @@
 package com.halildurmus.hotdeals.util;
 
+/**
+ * A utility class with useful function(s) to make it easier to work with {@code Enum}s.
+ */
 public class EnumUtil {
 
+  /**
+   * Checks whether the given {@code value} exists in the given {@code enumClass}.
+   *
+   * @param value     the {@code enum} value to be tested
+   * @param enumClass the {@code enum Class} to be checked eq. {@code DealStatus.class}
+   * @return {@code true} if the {@code value} exists in the {@code enumClass}; {@code false}
+   * otherwise.
+   */
   public static <E extends Enum<E>> boolean isInEnum(String value, Class<E> enumClass) {
     for (E e : enumClass.getEnumConstants()) {
       if (e.name().equals(value)) {
@@ -11,4 +22,5 @@ public class EnumUtil {
 
     return false;
   }
+
 }
