06ebc17f4e4a4b39d518de2727045dc33a2f3e10
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-01 23:05:31 +0300

    Add documentation to FakerUtil

--- src/main/java/com/halildurmus/hotdeals/util/FakerUtil.java
+++ src/main/java/com/halildurmus/hotdeals/util/FakerUtil.java
@@ -3,11 +3,19 @@ package com.halildurmus.hotdeals.util;
 import com.github.javafaker.Faker;
 import org.springframework.stereotype.Component;
 
+/**
+ * A utility class with useful function(s) to make it easier to work with {@code Faker}
+ */
 @Component
 public class FakerUtil {
 
   private final Faker faker = new Faker();
 
+  /**
+   * Generates a random nickname using {@code Faker}
+   *
+   * @return a random nickname
+   */
   public String generateNickname() {
     final String prefix = faker.superhero().prefix();
     final String firstName = faker.name().firstName();
