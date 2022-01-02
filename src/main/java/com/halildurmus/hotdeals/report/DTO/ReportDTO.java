ab78ed51613028231c622997c6edd27d4686306e
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-02 17:10:32 +0300

    Add missing @NoArgsConstructor annotation

--- src/main/java/com/halildurmus/hotdeals/report/DTO/ReportDTO.java
+++ src/main/java/com/halildurmus/hotdeals/report/DTO/ReportDTO.java
@@ -1,10 +1,12 @@
 package com.halildurmus.hotdeals.report.DTO;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 public class ReportDTO {
 
   private String message;
--- src/main/java/com/halildurmus/hotdeals/report/deal/DTO/DealReportPostDTO.java
+++ src/main/java/com/halildurmus/hotdeals/report/deal/DTO/DealReportPostDTO.java
@@ -6,11 +6,13 @@ import java.util.EnumSet;
 import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
+@NoArgsConstructor
 public class DealReportPostDTO extends ReportDTO {
 
   @NotEmpty
