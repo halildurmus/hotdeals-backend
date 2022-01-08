5916d0985a1f434e3f191f5a47b9da6b926ef7bb
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-08 21:49:21 +0300

    Update DummyStores

--- src/test/java/com/halildurmus/hotdeals/store/dummy/DummyStores.java
+++ src/test/java/com/halildurmus/hotdeals/store/dummy/DummyStores.java
@@ -4,6 +4,12 @@ import com.halildurmus.hotdeals.store.Store;
 
 public class DummyStores {
 
-  public static Store store1 = new Store("Amazon", "http://www.gravatar.com/avatar");
+  public static Store store1 = Store.builder().name("Amazon")
+      .logo("http://www.gravatar.com/avatar").build();
+
+  public static Store storeWithId = Store.builder()
+      .id("5fbe790ec6f0b32014074bb1")
+      .name("Amazon")
+      .logo("http://www.gravatar.com/avatar").build();
 
 }
