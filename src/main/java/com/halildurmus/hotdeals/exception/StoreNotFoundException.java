da8a35f8fe2c90a7e55395574ac7bbdaed0e20e2
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-08 21:48:01 +0300

    Create StoreNotFoundException

---
+++ src/main/java/com/halildurmus/hotdeals/exception/StoreNotFoundException.java
@@ -0,0 +1,13 @@
+package com.halildurmus.hotdeals.exception;
+
+public class StoreNotFoundException extends RuntimeException {
+
+  public StoreNotFoundException() {
+    super("Store not found");
+  }
+
+  public StoreNotFoundException(String message) {
+    super(message);
+  }
+
+}
