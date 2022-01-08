b7c231a2300a3f40ccda2d9cd8f50968b52acea7
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-08 16:20:35 +0300

    Refactor CategoryController

--- src/main/java/com/halildurmus/hotdeals/category/CategoryController.java
+++ src/main/java/com/halildurmus/hotdeals/category/CategoryController.java
@@ -2,26 +2,30 @@ package com.halildurmus.hotdeals.category;
 
 import com.halildurmus.hotdeals.category.DTO.CategoryGetDTO;
 import com.halildurmus.hotdeals.category.DTO.CategoryPostDTO;
+import com.halildurmus.hotdeals.exception.CategoryNotFoundException;
 import com.halildurmus.hotdeals.mapstruct.MapStructMapper;
 import com.halildurmus.hotdeals.util.ObjectIdConstraint;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-@RepositoryRestController
+@RestController
+@RequestMapping("/categories")
 @Validated
 public class CategoryController {
 
@@ -31,37 +35,48 @@ public class CategoryController {
   @Autowired
   private CategoryService service;
 
-  @GetMapping("/categories")
-  public ResponseEntity<List<CategoryGetDTO>> getCategories(Pageable pageable) {
+  @GetMapping
+  public List<CategoryGetDTO> getCategories(Pageable pageable) {
     final Page<Category> categories = service.findAll(pageable);
-    final List<CategoryGetDTO> categoryGetDTOs = categories.getContent().stream()
-        .map(category -> mapStructMapper.categoryToCategoryGetDTO(category)).collect(
-            Collectors.toList());
 
-    return ResponseEntity.ok(categoryGetDTOs);
+    return categories.getContent().stream()
+        .map(mapStructMapper::categoryToCategoryGetDTO)
+        .collect(Collectors.toList());
   }
 
-  @GetMapping("/categories/{id}")
-  public ResponseEntity<CategoryGetDTO> getCategory(@ObjectIdConstraint @PathVariable String id) {
-    final Optional<Category> category = service.findById(id);
-    if (category.isEmpty()) {
-      return ResponseEntity.notFound().build();
-    }
+  @GetMapping("/{id}")
+  public CategoryGetDTO getCategory(@ObjectIdConstraint @PathVariable String id) {
+    final Category category = service.findById(id).orElseThrow(CategoryNotFoundException::new);
 
-    return ResponseEntity.ok().body(mapStructMapper.categoryToCategoryGetDTO(category.get()));
+    return mapStructMapper.categoryToCategoryGetDTO(category);
   }
 
-  @PostMapping("/categories")
-  public ResponseEntity<CategoryGetDTO> createCategory(
-      @Valid @RequestBody CategoryPostDTO categoryPostDTO) {
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public CategoryGetDTO createCategory(@Valid @RequestBody CategoryPostDTO categoryPostDTO) {
     if (!categoryPostDTO.getNames().containsKey("en")) {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
           "The category name must have an English translation!");
     }
-    final Category category = service.save(
+    final Category category = service.create(
         mapStructMapper.categoryPostDTOCategory(categoryPostDTO));
 
-    return ResponseEntity.status(201).body(mapStructMapper.categoryToCategoryGetDTO(category));
+    return mapStructMapper.categoryToCategoryGetDTO(category);
+  }
+
+  @PutMapping("/{id}")
+  public CategoryGetDTO updateCategory(@ObjectIdConstraint @PathVariable String id,
+      @Valid @RequestBody CategoryPostDTO categoryPostDTO) {
+    final Category category = mapStructMapper.categoryPostDTOCategory(categoryPostDTO);
+    category.setId(id);
+
+    return mapStructMapper.categoryToCategoryGetDTO(service.update(category));
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteCategory(@ObjectIdConstraint @PathVariable String id) {
+    service.delete(id);
   }
 
 }
