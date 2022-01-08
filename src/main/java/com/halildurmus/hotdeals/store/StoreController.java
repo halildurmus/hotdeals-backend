f1a64c00ed0b885a51a4adfdaa2da89045df3023
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-08 21:50:24 +0300

    Refactor StoreController

--- src/main/java/com/halildurmus/hotdeals/store/StoreController.java
+++ src/main/java/com/halildurmus/hotdeals/store/StoreController.java
@@ -1,25 +1,30 @@
 package com.halildurmus.hotdeals.store;
 
+import com.halildurmus.hotdeals.exception.StoreNotFoundException;
 import com.halildurmus.hotdeals.mapstruct.MapStructMapper;
 import com.halildurmus.hotdeals.store.DTO.StoreGetDTO;
 import com.halildurmus.hotdeals.store.DTO.StorePostDTO;
 import com.halildurmus.hotdeals.util.ObjectIdConstraint;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@RepositoryRestController
+@RestController
+@RequestMapping("/stores")
 @Validated
 public class StoreController {
 
@@ -29,31 +34,43 @@ public class StoreController {
   @Autowired
   private StoreService service;
 
-  @GetMapping("/stores")
-  public ResponseEntity<List<StoreGetDTO>> getStores(Pageable pageable) {
+  @GetMapping
+  public List<StoreGetDTO> getStores(Pageable pageable) {
     final Page<Store> stores = service.findAll(pageable);
-    final List<StoreGetDTO> storeGetDTOs = stores.getContent().stream()
-        .map(store -> mapStructMapper.storeToStoreGetDTO(store)).collect(
-            Collectors.toList());
 
-    return ResponseEntity.ok(storeGetDTOs);
+    return stores.getContent().stream()
+        .map(mapStructMapper::storeToStoreGetDTO)
+        .collect(Collectors.toList());
   }
 
-  @GetMapping("/stores/{id}")
-  public ResponseEntity<StoreGetDTO> getStore(@ObjectIdConstraint @PathVariable String id) {
-    final Optional<Store> store = service.findById(id);
-    if (store.isEmpty()) {
-      return ResponseEntity.notFound().build();
-    }
+  @GetMapping("/{id}")
+  public StoreGetDTO getStore(@ObjectIdConstraint @PathVariable String id) {
+    final Store store = service.findById(id).orElseThrow(StoreNotFoundException::new);
 
-    return ResponseEntity.ok().body(mapStructMapper.storeToStoreGetDTO(store.get()));
+    return mapStructMapper.storeToStoreGetDTO(store);
   }
 
-  @PostMapping("/stores")
-  public ResponseEntity<StoreGetDTO> createStore(@Valid @RequestBody StorePostDTO storePostDTO) {
-    final Store store = service.save(mapStructMapper.storePostDTOStore(storePostDTO));
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public StoreGetDTO createStore(@Valid @RequestBody StorePostDTO storePostDTO) {
+    final Store store = service.create(mapStructMapper.storePostDTOStore(storePostDTO));
 
-    return ResponseEntity.status(201).body(mapStructMapper.storeToStoreGetDTO(store));
+    return mapStructMapper.storeToStoreGetDTO(store);
+  }
+
+  @PutMapping("/{id}")
+  public StoreGetDTO updateStore(@ObjectIdConstraint @PathVariable String id,
+      @Valid @RequestBody StorePostDTO storePostDTO) {
+    final Store store = mapStructMapper.storePostDTOStore(storePostDTO);
+    store.setId(id);
+
+    return mapStructMapper.storeToStoreGetDTO(service.update(store));
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteStore(@ObjectIdConstraint @PathVariable String id) {
+    service.delete(id);
   }
 
 }
