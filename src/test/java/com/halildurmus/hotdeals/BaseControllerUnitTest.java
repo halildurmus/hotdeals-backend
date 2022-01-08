726e6981ee814a67a6ad2e0ca74a1cac259c6dab
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-08 16:22:33 +0300

    Create BaseControllerUnitTest

---
+++ src/test/java/com/halildurmus/hotdeals/BaseControllerUnitTest.java
@@ -0,0 +1,23 @@
+package com.halildurmus.hotdeals;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(
+    useDefaultFilters = false,
+    excludeAutoConfiguration = SecurityAutoConfiguration.class)
+@AutoConfigureJsonTesters
+public abstract class BaseControllerUnitTest {
+
+  // See https://stackoverflow.com/questions/53514532/
+  protected <T> T asParsedJson(Object object) throws JsonProcessingException {
+    final String json = new ObjectMapper().writeValueAsString(object);
+
+    return JsonPath.read(json, "$");
+  }
+
+}
