ee67defdd4398981f33b87b89396c191149816f0
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-08 16:16:34 +0300

    Rename integration tests

--- src/test/java/com/halildurmus/hotdeals/category/CategoryIntegrationTest.java
+++ src/test/java/com/halildurmus/hotdeals/category/CategoryIntegrationTest.java
@@ -5,28 +5,28 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import com.halildurmus.hotdeals.BaseIntegrationTest;
+import com.halildurmus.hotdeals.category.DTO.CategoryPostDTO;
 import com.halildurmus.hotdeals.category.dummy.DummyCategories;
+import com.halildurmus.hotdeals.mapstruct.MapStructMapperImpl;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
-@ActiveProfiles("integration-test")
 @AutoConfigureJsonTesters
 @AutoConfigureMockMvc(addFilters = false)
 public class CategoryControllerIntegrationTest extends BaseIntegrationTest {
+
+  private final MapStructMapperImpl mapStructMapper = new MapStructMapperImpl();
 
   @Autowired
   private MongoTemplate mongoTemplate;
@@ -35,7 +35,7 @@ public class CategoryControllerIntegrationTest extends BaseIntegrationTest {
   private MockMvc mvc;
 
   @Autowired
-  private JacksonTester<Category> json;
+  private JacksonTester<CategoryPostDTO> json;
 
   @AfterEach
   void cleanUp() {
@@ -45,23 +45,28 @@ public class CategoryControllerIntegrationTest extends BaseIntegrationTest {
   // TODO: Write test cases for validating mandatory fields.
 
   @Test
-  @DisplayName("POST /categories")
-  public void shouldCreateCategoryThenReturnCategory() throws Exception {
+  @DisplayName("POST /categories (success)")
+  public void shouldCreateCategory() throws Exception {
+    final CategoryPostDTO categoryPostDTO = mapStructMapper.categoryToCategoryPostDTO(
+        DummyCategories.category1);
     final RequestBuilder requestBuilder = MockMvcRequestBuilders
         .post("/categories")
         .accept(MediaType.APPLICATION_JSON)
-        .content(json.write(DummyCategories.category1).getJson())
+        .content(json.write(categoryPostDTO).getJson())
         .contentType(MediaType.APPLICATION_JSON);
 
     mvc.perform(requestBuilder).andExpect(MockMvcResultMatchers.status().isCreated())
         .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
         .andExpect(
-            jsonPath("$.names").value(equalTo(asParsedJson(DummyCategories.category1.getNames()))))
+            jsonPath("$.names").value(
+                equalTo(asParsedJson(DummyCategories.category1.getNames()))))
         .andExpect(jsonPath("$.parent").value(DummyCategories.category1.getParent()))
         .andExpect(jsonPath("$.category").value(DummyCategories.category1.getCategory()))
-        .andExpect(jsonPath("$.iconLigature").value(DummyCategories.category1.getIconLigature()))
         .andExpect(
-            jsonPath("$.iconFontFamily").value(DummyCategories.category1.getIconFontFamily()));
+            jsonPath("$.iconLigature").value(DummyCategories.category1.getIconLigature()))
+        .andExpect(
+            jsonPath("$.iconFontFamily").value(
+                DummyCategories.category1.getIconFontFamily()));
   }
 
   @Test
@@ -72,8 +77,7 @@ public class CategoryControllerIntegrationTest extends BaseIntegrationTest {
         .accept(MediaType.APPLICATION_JSON)
         .contentType(MediaType.APPLICATION_JSON);
 
-    mvc.perform(
-            requestBuilder)
+    mvc.perform(requestBuilder)
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
         .andExpect(jsonPath("$._embedded.categories", hasSize(0)));
