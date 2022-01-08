d95fac7ca944830bbc3a4a7cc5d0ca23415241e9
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-08 16:22:12 +0300

    Update BaseIntegrationTest

--- src/test/java/com/halildurmus/hotdeals/BaseIntegrationTest.java
+++ src/test/java/com/halildurmus/hotdeals/BaseIntegrationTest.java
@@ -3,6 +3,8 @@ package com.halildurmus.hotdeals;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -12,6 +14,8 @@ import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @ActiveProfiles("integration-test")
+@AutoConfigureJsonTesters
+@AutoConfigureMockMvc(addFilters = false)
 public abstract class BaseIntegrationTest {
 
   private static final int REDIS_PORT = 6379;
