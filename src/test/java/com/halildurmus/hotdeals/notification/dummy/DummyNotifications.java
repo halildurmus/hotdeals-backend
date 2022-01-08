366e2fd14d5823da18e9531fd120ac2b027f6512
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-08 22:14:22 +0300

    Create DummyNotifications

---
+++ src/test/java/com/halildurmus/hotdeals/notification/dummy/DummyNotifications.java
@@ -0,0 +1,13 @@
+package com.halildurmus.hotdeals.notification.dummy;
+
+import com.halildurmus.hotdeals.notification.Notification;
+import java.util.List;
+import java.util.Map;
+
+public class DummyNotifications {
+
+  public static Notification notification1 = Notification.builder()
+      .title("Title").body("Body").image("http://www.gravatar.com/avatar")
+      .data(Map.of("uid", "jsdf235u2342fsah")).tokens(List.of("423hjcs235")).build();
+
+}
