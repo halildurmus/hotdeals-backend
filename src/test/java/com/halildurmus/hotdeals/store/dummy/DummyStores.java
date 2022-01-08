7f71e06ddad139a0eec145592b448590c9156211
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-08 22:00:48 +0300

    Add new dummy store

--- src/test/java/com/halildurmus/hotdeals/store/dummy/DummyStores.java
+++ src/test/java/com/halildurmus/hotdeals/store/dummy/DummyStores.java
@@ -7,6 +7,9 @@ public class DummyStores {
   public static Store store1 = Store.builder().name("Amazon")
       .logo("http://www.gravatar.com/avatar").build();
 
+  public static Store store2 = Store.builder().name("Newegg")
+      .logo("http://www.gravatar.com/avatar").build();
+
   public static Store storeWithId = Store.builder()
       .id("5fbe790ec6f0b32014074bb1")
       .name("Amazon")
