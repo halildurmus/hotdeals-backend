3995f5d34808365c9f1bfbc9839e4ef70ba10623
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-10 18:49:46 +0300

    Update DummyUsers

--- src/test/java/com/halildurmus/hotdeals/user/dummy/DummyUsers.java
+++ src/test/java/com/halildurmus/hotdeals/user/dummy/DummyUsers.java
@@ -1,18 +1,23 @@
 package com.halildurmus.hotdeals.user.dummy;
 
 import com.halildurmus.hotdeals.user.User;
+import java.time.Instant;
 
 public class DummyUsers {
 
-  public static User user1 = User.builder().id("11111111").email("mike@spring.io")
-      .nickname("mike1234").avatar("http://www.gravatar.com/avatar").build();
-  public static User user2 = User.builder().id("22222222").email("duke@spring.io")
-      .nickname("duke1234").avatar("http://www.gravatar.com/avatar").build();
-  public static User user3 = User.builder().id("33333333").email("hannah@spring.io")
-      .nickname("hannah1234").avatar("http://www.gravatar.com/avatar").build();
-  public static User user4 = User.builder().id("44444444").email("harold@spring.io")
-      .nickname("harold1234").avatar("http://www.gravatar.com/avatar").build();
-  public static User user4WithoutNickname = User.builder().id("55555555")
-      .email("harold@spring.io").nickname("").avatar("http://www.gravatar.com/avatar").build();
+  public static User user1 = User.builder().id("5fbe790ec6f0b32014074bb1")
+      .email("mike@spring.io").nickname("mike1234")
+      .avatar("http://www.gravatar.com/avatar").build();
+  public static User user2 = User.builder().id("5fbe790ec6f0b32014074bb2")
+      .email("duke@spring.io").nickname("duke1234")
+      .avatar("http://www.gravatar.com/avatar").build();
+  public static User user3 = User.builder().id("5fbe790ec6f0b32014074bb3")
+      .email("hannah@spring.io").nickname("hannah1234")
+      .avatar("http://www.gravatar.com/avatar").build();
+  public static User user4 = User.builder().id("5fbe790ec6f0b32014074bb4")
+      .uid("432hdf5324sf23").avatar("http://www.gravatar.com/avatar")
+      .nickname("harold1234").createdAt(Instant.now()).build();
+  public static User user4WithoutNickname = User.builder().uid("432hdf5324sf23")
+      .email("harold@spring.io").avatar("http://www.gravatar.com/avatar").build();
 
 }
