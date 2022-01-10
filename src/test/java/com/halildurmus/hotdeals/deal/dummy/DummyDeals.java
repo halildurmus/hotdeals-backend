99f638f03cb06cfafe65176833563ee69ca37a3a
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-11 02:17:31 +0300

    Add new deal to DummyDeals

--- src/test/java/com/halildurmus/hotdeals/deal/dummy/DummyDeals.java
+++ src/test/java/com/halildurmus/hotdeals/deal/dummy/DummyDeals.java
@@ -1,6 +1,10 @@
 package com.halildurmus.hotdeals.deal.dummy;
 
 import com.halildurmus.hotdeals.deal.Deal;
+import com.halildurmus.hotdeals.deal.DealStatus;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
 import org.bson.types.ObjectId;
 
 public class DummyDeals {
@@ -9,5 +13,14 @@ public class DummyDeals {
       .description("12TB WD External HDD\n$190").originalPrice(150.0).price(140.0)
       .store(new ObjectId("5fbe790ec6f0b32014074bb1")).category("/electronics")
       .coverPhoto("http://www.gravatar.com/avatar").dealUrl("https://www.amazon.com/item").build();
+
+  public static Deal deal1GetDto = Deal.builder().id("5fbe720ec6f0b32014074bb0")
+      .postedBy(new ObjectId("5fbe790ec6f0b32014074bb3"))
+      .dealScore(0).views(0).status(DealStatus.ACTIVE).createdAt(Instant.now())
+      .title("12TB WD My Book Desktop External HDD").photos(List.of())
+      .description("12TB WD External HDD\n$190").originalPrice(150.0).price(140.0)
+      .store(new ObjectId("5fbe790ec6f0b32014074bb1")).category("/electronics")
+      .coverPhoto("http://www.gravatar.com/avatar").dealUrl("https://www.amazon.com/item")
+      .upvoters(new HashSet<>(List.of())).downvoters(new HashSet<>(List.of())).build();
 
 }
