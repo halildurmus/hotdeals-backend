7b5f884ca9e40b12e8298421e12b2a9ac472a2c0
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-12 16:44:18 +0300

    Fix UserIntegrationTest

--- src/test/java/com/halildurmus/hotdeals/user/UserIntegrationTest.java
+++ src/test/java/com/halildurmus/hotdeals/user/UserIntegrationTest.java
@@ -1,10 +1,14 @@
 package com.halildurmus.hotdeals.user;
 
-import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.halildurmus.hotdeals.BaseIntegrationTest;
 import com.halildurmus.hotdeals.user.dummy.DummyUsers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,10 +19,8 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-public class UserIntegrationTest {
+public class UserIntegrationTest extends BaseIntegrationTest {
 
   @Autowired
   private MongoTemplate mongoTemplate;
@@ -39,47 +41,63 @@ public class UserIntegrationTest {
   @Test
   @DisplayName("POST /create")
   public void shouldCreateUserThenReturnUser() throws Exception {
-    final RequestBuilder requestBuilder = MockMvcRequestBuilders
-        .post("/users")
+    final User user = DummyUsers.user1;
+    final RequestBuilder requestBuilder = post("/users")
         .accept(MediaType.APPLICATION_JSON)
-        .content(json.write(DummyUsers.user1).getJson())
+        .content(json.write(user).getJson())
         .contentType(MediaType.APPLICATION_JSON);
 
-    mvc.perform(requestBuilder).andExpect(MockMvcResultMatchers.status().isCreated())
-        .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-        .andExpect(jsonPath("$.nickname").value(not(empty())));
+    mvc.perform(requestBuilder)
+        .andExpect(status().isCreated())
+        .andExpect(content().contentType("application/json"))
+        .andExpect(jsonPath("$.*", hasSize(5)))
+        .andExpect(jsonPath("$.id").isNotEmpty())
+        .andExpect(jsonPath("$.uid").value(user.getUid()))
+        .andExpect(jsonPath("$.avatar").value(user.getAvatar()))
+        .andExpect(jsonPath("$.nickname").isNotEmpty())
+        .andExpect(jsonPath("$.createdAt").isNotEmpty());
   }
 
   @Test
   @DisplayName("GET /users (returns empty)")
-  public void shouldReturnEmptyArray() throws Exception {
-    final RequestBuilder requestBuilder = MockMvcRequestBuilders
-        .get("/users")
+  public void getUsersReturnsEmptyArray() throws Exception {
+    final RequestBuilder requestBuilder = get("/users")
         .accept(MediaType.APPLICATION_JSON)
         .contentType(MediaType.APPLICATION_JSON);
 
-    mvc.perform(
-            requestBuilder)
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-        .andExpect(jsonPath("$._embedded.users", hasSize(0)));
+    mvc.perform(requestBuilder)
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json"))
+        .andExpect(jsonPath("$.content", hasSize(0)));
   }
 
   @Test
   @DisplayName("GET /users (returns 1 user)")
-  public void shouldReturnOneUserInArray() throws Exception {
-    mongoTemplate.insert(DummyUsers.user1);
-
-    final RequestBuilder requestBuilder = MockMvcRequestBuilders
-        .get("/users")
+  public void getUsersReturnsOneUser() throws Exception {
+    final User user = DummyUsers.user1;
+    mongoTemplate.insert(user);
+    final RequestBuilder requestBuilder = get("/users")
         .accept(MediaType.APPLICATION_JSON)
         .contentType(MediaType.APPLICATION_JSON);
 
-    mvc.perform(
-            requestBuilder)
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-        .andExpect(jsonPath("$._embedded.users", hasSize(1)))
-        .andExpect(jsonPath("$._embedded.users[0].email").value(DummyUsers.user1.getEmail()));
+    mvc.perform(requestBuilder)
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json"))
+        .andExpect(jsonPath("$.content", hasSize(1)))
+        .andExpect(jsonPath("$.content[0].*", hasSize(10)))
+        .andExpect(jsonPath("$.content[0].id").isNotEmpty())
+        .andExpect(jsonPath("$.content[0].uid").value(user.getUid()))
+        .andExpect(jsonPath("$.content[0].email").value(user.getEmail()))
+        .andExpect(jsonPath("$.content[0].avatar").value(user.getAvatar()))
+        .andExpect(jsonPath("$.content[0].nickname").value(user.getNickname()))
+        .andExpect(
+            jsonPath("$.content[0].favorites").value(equalTo(asParsedJson(user.getFavorites()))))
+        .andExpect(jsonPath("$.content[0].blockedUsers").value(
+            equalTo(asParsedJson(user.getBlockedUsers()))))
+        .andExpect(
+            jsonPath("$.content[0].fcmTokens").value(equalTo(asParsedJson(user.getFcmTokens()))))
+        .andExpect(jsonPath("$.content[0].createdAt").isNotEmpty())
+        .andExpect(jsonPath("$.content[0].updatedAt").isNotEmpty());
   }
+
 }
