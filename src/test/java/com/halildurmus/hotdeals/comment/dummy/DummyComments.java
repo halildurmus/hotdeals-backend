5b124b05c59c852f15e31a58c27d66468af2c661
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-12 03:10:25 +0300

    Update dummy comment

--- src/test/java/com/halildurmus/hotdeals/comment/dummy/DummyComments.java
+++ src/test/java/com/halildurmus/hotdeals/comment/dummy/DummyComments.java
@@ -1,12 +1,17 @@
 package com.halildurmus.hotdeals.comment.dummy;
 
 import com.halildurmus.hotdeals.comment.Comment;
+import com.halildurmus.hotdeals.user.dummy.DummyUsers;
+import java.time.Instant;
 import org.bson.types.ObjectId;
 
 public class DummyComments {
 
   public static Comment comment1 = Comment.builder()
+      .id("5fbe790ec6f0b32014074bb1")
+      .postedBy(DummyUsers.user1)
       .dealId(new ObjectId("5fbe790ec6f0b32014074bb2"))
-      .message("Lorem dolor sit amet").build();
+      .message("Lorem dolor sit amet")
+      .createdAt(Instant.now()).build();
 
 }
