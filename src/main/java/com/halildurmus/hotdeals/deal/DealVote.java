0330f64efaf381a99a37dfc39d8bc2481bef0cc8
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-12 03:08:49 +0300

    Create DealVote

---
+++ src/main/java/com/halildurmus/hotdeals/deal/DealVote.java
@@ -0,0 +1,18 @@
+package com.halildurmus.hotdeals.deal;
+
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DealVote {
+
+  @NotNull
+  private DealVoteType voteType;
+
+}
