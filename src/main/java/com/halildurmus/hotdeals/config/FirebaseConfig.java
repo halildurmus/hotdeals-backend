4d1abda5293fb27dcb4f635ac565e391c9482e47
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-13 22:41:29 +0300

    Remove unused Beans

--- src/main/java/com/halildurmus/hotdeals/config/FirebaseConfig.java
+++ src/main/java/com/halildurmus/hotdeals/config/FirebaseConfig.java
@@ -1,14 +1,10 @@
 package com.halildurmus.hotdeals.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.halildurmus.hotdeals.security.models.SecurityProperties;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,26 +38,8 @@ public class FirebaseConfig {
   }
 
   @Bean
-  public FirebaseDatabase firebaseDatabase() {
-    return FirebaseDatabase.getInstance();
-  }
-
-  @Bean
-  public Firestore getDatabase() throws IOException {
-    FirestoreOptions firestoreOptions = FirestoreOptions.newBuilder()
-        .setCredentials(GoogleCredentials.getApplicationDefault()).build();
-
-    return firestoreOptions.getService();
-  }
-
-  @Bean
   public FirebaseMessaging getMessaging() throws IOException {
     return FirebaseMessaging.getInstance(getFirebaseApp());
-  }
-
-  @Bean
-  public FirebaseRemoteConfig getRemoteConfig() throws IOException {
-    return FirebaseRemoteConfig.getInstance(getFirebaseApp());
   }
 
 }
