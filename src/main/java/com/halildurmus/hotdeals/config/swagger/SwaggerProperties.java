4150f4f439f7af04fccf5e9d81fccf14f7ddfa8f
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-16 18:58:31 +0300

    Create SwaggerProperties and SwaggerContact

---
+++ src/main/java/com/halildurmus/hotdeals/config/swagger/SwaggerProperties.java
@@ -0,0 +1,17 @@
+package com.halildurmus.hotdeals.config.swagger;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("swagger")
+@Data
+class SwaggerProperties {
+
+  private SwaggerContact contact;
+  private String description;
+  private String title;
+  private String version;
+
+}
