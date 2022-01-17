ddff83becb02b32cef7474319cb76edd8c8d53ed
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-17 17:26:50 +0300

    Document RoleController

--- src/main/java/com/halildurmus/hotdeals/security/role/RoleController.java
+++ src/main/java/com/halildurmus/hotdeals/security/role/RoleController.java
@@ -1,5 +1,11 @@
 package com.halildurmus.hotdeals.security.role;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
 import javax.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,22 +16,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "roles")
 @RestController
 @RequestMapping("/roles")
-@IsSuper
 public class RoleController {
 
   @Autowired
   private RoleService service;
 
   @PutMapping
-  public void addRole(@RequestParam @NotBlank String uid, @RequestParam Role role) {
+  @IsSuper
+  @ApiOperation(value = "Adds the given role to the given user in the Firebase", authorizations = @Authorization("Bearer"))
+  @ApiResponses({
+      @ApiResponse(code = 400, message = "Bad Request"),
+      @ApiResponse(code = 401, message = "Unauthorized"),
+      @ApiResponse(code = 403, message = "Forbidden")
+  })
+  public void addRole(
+      @ApiParam("String representation of the Firebase User ID. e.g. 'ndj2KkbGwIUbfIUH2BT6700AQ832'")
+      @RequestParam @NotBlank String uid, @ApiParam("User role") @RequestParam Role role) {
     service.add(uid, role);
   }
 
   @DeleteMapping
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteRole(@RequestParam @NotBlank String uid, @RequestParam Role role) {
+  @IsSuper
+  @ApiOperation(value = "Deletes the given role from the given user in the Firebase", authorizations = @Authorization("Bearer"))
+  @ApiResponses({
+      @ApiResponse(code = 400, message = "Bad Request"),
+      @ApiResponse(code = 401, message = "Unauthorized"),
+      @ApiResponse(code = 403, message = "Forbidden")
+  })
+  public void deleteRole(
+      @ApiParam("String representation of the Firebase User ID. e.g. 'ndj2KkbGwIUbfIUH2BT6700AQ832'")
+      @RequestParam @NotBlank String uid, @ApiParam("User role") @RequestParam Role role) {
     service.delete(uid, role);
   }
 
