3e91147047066c3be601e445f4fd8e7bf88a78b0
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-17 17:26:22 +0300

    Document NotificationController

--- src/main/java/com/halildurmus/hotdeals/notification/NotificationController.java
+++ src/main/java/com/halildurmus/hotdeals/notification/NotificationController.java
@@ -1,5 +1,12 @@
 package com.halildurmus.hotdeals.notification;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
+import io.swagger.annotations.Example;
+import io.swagger.annotations.ExampleProperty;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+@Api(tags = "notifications")
 @RestController
 @RequestMapping("/notifications")
 @Validated
@@ -22,6 +30,12 @@ public class NotificationController {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
+  @ApiOperation(value = "Sends a push notification using FCM(Firebase Cloud Messaging)", notes = "<b>*</b>(<b>title</b> or <b>titleLocKey</b>) and (<b>body</b> or <b>bodyLocKey</b>) parameters are required", authorizations = @Authorization("Bearer"))
+  @ApiResponses({
+      @ApiResponse(code = 201, message = "Push notification sent", examples = @Example(@ExampleProperty("1"))),
+      @ApiResponse(code = 400, message = "Bad Request"),
+      @ApiResponse(code = 401, message = "Unauthorized")
+  })
   public Integer sendNotification(@Valid @RequestBody Notification notification) {
     if (ObjectUtils.isEmpty(notification.getTitle()) && ObjectUtils.isEmpty(
         notification.getTitleLocKey())) {
