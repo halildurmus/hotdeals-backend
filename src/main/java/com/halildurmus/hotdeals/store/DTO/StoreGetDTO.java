9b0e35205edc008519f0c6f5a1b7092677dde90c
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-17 17:26:02 +0300

    Document Store API Models

--- src/main/java/com/halildurmus/hotdeals/store/DTO/StoreGetDTO.java
+++ src/main/java/com/halildurmus/hotdeals/store/DTO/StoreGetDTO.java
@@ -1,16 +1,22 @@
 package com.halildurmus.hotdeals.store.DTO;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
+@ApiModel("StoreGetDTO")
 @Data
 @Builder
 public class StoreGetDTO {
 
+  @ApiModelProperty(value = "Store id", position = 1, example = "5fbe790ec6f0b32014074bb1")
   private final String id;
 
+  @ApiModelProperty(value = "Store name", position = 2, example = "Amazon")
   private final String name;
 
+  @ApiModelProperty(value = "Store logo URL", position = 3, example = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png")
   private final String logo;
 
 }
--- src/main/java/com/halildurmus/hotdeals/store/DTO/StorePostDTO.java
+++ src/main/java/com/halildurmus/hotdeals/store/DTO/StorePostDTO.java
@@ -1,17 +1,22 @@
 package com.halildurmus.hotdeals.store.DTO;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 
+@ApiModel("StorePostDTO")
 @Data
 @Builder
 public class StorePostDTO {
 
+  @ApiModelProperty(value = "Store name", position = 1, example = "Amazon", required = true)
   @NotBlank
   private final String name;
 
+  @ApiModelProperty(value = "Store logo URL", position = 2, example = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png", required = true)
   @URL
   @NotBlank
   private final String logo;
