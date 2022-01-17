baf403cda2b3408a3f4c09e8d37dade56c642a9e
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-17 17:24:25 +0300

    Guard methods with @IsSuper

--- src/main/java/com/halildurmus/hotdeals/category/CategoryService.java
+++ src/main/java/com/halildurmus/hotdeals/category/CategoryService.java
@@ -1,5 +1,6 @@
 package com.halildurmus.hotdeals.category;
 
+import com.halildurmus.hotdeals.security.role.IsSuper;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,12 +9,16 @@ public interface CategoryService {
 
   Page<Category> findAll(Pageable pageable);
 
+  @IsSuper
   Optional<Category> findById(String id);
 
+  @IsSuper
   Category create(Category category);
 
+  @IsSuper
   Category update(Category category);
 
+  @IsSuper
   void delete(String id);
 
 }
