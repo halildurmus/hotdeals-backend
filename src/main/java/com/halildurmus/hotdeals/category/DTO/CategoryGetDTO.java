131bd239bd0c161178e5ef411a14b0ad0a458168
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-17 17:25:52 +0300

    Document Category API Models

--- src/main/java/com/halildurmus/hotdeals/category/DTO/CategoryGetDTO.java
+++ src/main/java/com/halildurmus/hotdeals/category/DTO/CategoryGetDTO.java
@@ -1,23 +1,32 @@
 package com.halildurmus.hotdeals.category.DTO;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.Map;
 import lombok.Builder;
 import lombok.Data;
 
+@ApiModel("CategoryGetDTO")
 @Data
 @Builder
 public class CategoryGetDTO {
 
+  @ApiModelProperty(value = "Category id", position = 1, example = "5fbe790ec6f0b32014074bb1")
   private final String id;
 
+  @ApiModelProperty(value = "Category names", position = 2, example = "{\"en\": \"Computers\", \"tr\": \"Bilgisayar\"}")
   private final Map<String, String> names;
 
+  @ApiModelProperty(value = "Parent category path", position = 3, example = "/")
   private final String parent;
 
+  @ApiModelProperty(value = "Category path", position = 4, example = "/computers")
   private final String category;
 
+  @ApiModelProperty(value = "Category icon ligature", position = 5, example = "computer")
   private final String iconLigature;
 
+  @ApiModelProperty(value = "Category icon font family", position = 6, example = "MaterialIcons")
   private final String iconFontFamily;
 
 }
--- src/main/java/com/halildurmus/hotdeals/category/DTO/CategoryPostDTO.java
+++ src/main/java/com/halildurmus/hotdeals/category/DTO/CategoryPostDTO.java
@@ -1,27 +1,35 @@
 package com.halildurmus.hotdeals.category.DTO;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.Map;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
+@ApiModel("CategoryPostDTO")
 @Data
 @Builder
 public class CategoryPostDTO {
 
+  @ApiModelProperty(value = "Category names", position = 1, example = "{\"en\": \"Computers\", \"tr\": \"Bilgisayar\"}", required = true)
   @NotNull
   private final Map<String, String> names;
 
+  @ApiModelProperty(value = "Parent category path", position = 2, example = "/", required = true)
   @NotBlank
   private final String parent;
 
+  @ApiModelProperty(value = "Category path", position = 3, example = "/computers", required = true)
   @NotBlank
   private final String category;
 
+  @ApiModelProperty(value = "Category icon ligature", position = 4, example = "computer", required = true)
   @NotBlank
   private final String iconLigature;
 
+  @ApiModelProperty(value = "Category icon font family", position = 5, example = "MaterialIcons", required = true)
   @NotBlank
   private final String iconFontFamily;
 
