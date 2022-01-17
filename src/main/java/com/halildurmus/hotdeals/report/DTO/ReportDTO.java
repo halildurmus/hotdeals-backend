07f18ebd30a96144d6ade2f6b866038df2bfcd88
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-17 18:41:28 +0300

    Document ReportDTO and UserReportPostDTO API Models

--- src/main/java/com/halildurmus/hotdeals/report/DTO/ReportDTO.java
+++ src/main/java/com/halildurmus/hotdeals/report/DTO/ReportDTO.java
@@ -1,14 +1,18 @@
 package com.halildurmus.hotdeals.report.DTO;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@ApiModel("ReportDTO")
 @Data
 @SuperBuilder
 @NoArgsConstructor
 public class ReportDTO {
 
+  @ApiModelProperty(value = "Report message", position = 1, example = "The user is harassing me")
   private String message;
 
 }
--- src/main/java/com/halildurmus/hotdeals/report/user/DTO/UserReportPostDTO.java
+++ src/main/java/com/halildurmus/hotdeals/report/user/DTO/UserReportPostDTO.java
@@ -2,6 +2,8 @@ package com.halildurmus.hotdeals.report.user.DTO;
 
 import com.halildurmus.hotdeals.report.DTO.ReportDTO;
 import com.halildurmus.hotdeals.report.user.UserReportReason;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.EnumSet;
 import javax.validation.constraints.NotEmpty;
 import lombok.Data;
@@ -9,12 +11,14 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@ApiModel("UserReportPostDTO")
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
 @NoArgsConstructor
 public class UserReportPostDTO extends ReportDTO {
 
+  @ApiModelProperty(value = "User report reasons", position = 1, example = "[HARASSING,OTHER]")
   @NotEmpty
   private EnumSet<UserReportReason> reasons;
 
