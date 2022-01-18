a019f47ac82668154a926c65ad661326787c75d2
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-01-19 01:16:16 +0300

    Remove @Schema

--- src/main/java/com/halildurmus/hotdeals/report/user/DTO/UserReportPostDTO.java
+++ src/main/java/com/halildurmus/hotdeals/report/user/DTO/UserReportPostDTO.java
@@ -2,7 +2,6 @@ package com.halildurmus.hotdeals.report.user.DTO;
 
 import com.halildurmus.hotdeals.report.DTO.ReportDTO;
 import com.halildurmus.hotdeals.report.user.UserReportReason;
-import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.EnumSet;
 import javax.validation.constraints.NotEmpty;
 import lombok.Data;
@@ -10,14 +9,12 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-@Schema(name = "UserReportPostDTO")
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
 @NoArgsConstructor
 public class UserReportPostDTO extends ReportDTO {
 
-  @Schema(description = "User report reasons", example = "[HARASSING,OTHER]")
   @NotEmpty
   private EnumSet<UserReportReason> reasons;
 
