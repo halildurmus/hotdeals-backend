1e00ff6476769ec43d90bf36d254dd1adc93c7d2
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-02-19 02:36:31 +0300

    Use @Configuration instead of @Component

--- src/main/java/com/halildurmus/hotdeals/security/models/SecurityProperties.java
+++ src/main/java/com/halildurmus/hotdeals/security/models/SecurityProperties.java
@@ -3,9 +3,9 @@ package com.halildurmus.hotdeals.security.models;
 import java.util.List;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 @ConfigurationProperties("security")
 @Data
 public class SecurityProperties {
