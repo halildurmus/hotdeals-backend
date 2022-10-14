4629d72ccb1deca86f4982ed4e7b912dd37e14ee
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-10-14 11:06:11 +0300

    Use embedded-redis for integration tests

--- src/test/java/com/halildurmus/hotdeals/BaseIntegrationTest.java
+++ src/test/java/com/halildurmus/hotdeals/BaseIntegrationTest.java
@@ -7,33 +7,12 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @ActiveProfiles("integration-test")
 @AutoConfigureJsonTesters
 @AutoConfigureMockMvc(addFilters = false)
 public abstract class BaseIntegrationTest {
-
-  private static final int REDIS_PORT = 6379;
-
-  private static final DockerImageName REDIS_IMAGE = DockerImageName.parse("redis:latest");
-
-  private static final GenericContainer<?> redis;
-
-  static {
-    redis = new GenericContainer<>(REDIS_IMAGE).withExposedPorts(REDIS_PORT);
-    redis.start();
-  }
-
-  @DynamicPropertySource
-  static void properties(DynamicPropertyRegistry registry) {
-    registry.add("spring.redis.host", redis::getHost);
-    registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
-  }
 
   // See https://stackoverflow.com/questions/53514532/
   protected <T> T asParsedJson(Object object) throws JsonProcessingException {
